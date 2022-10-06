8c47d8bd2efb4fb878fc8db61c6e3f499c9081de
ktyoung <ktyoung0507@gmail.com>
2022-10-06 12:39:28 +0900

    반복문 예제 1

---
+++ ControlFlowFor/app/src/main/java/com/ktyoung0507/controlflowfor/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.ktyoung0507.controlflowfor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+        for(index in 1..10) {
+            Log.d("For", "현재 숫자는 ${index}")
+        }
+        */
+
+        /*
+        var array = arrayOf("JAN", "FEB", "MAR", "APR", "MAY", "JUN")
+        for(index in 0 until array.size) {
+            Log.d("For", "현재 월은 ${array.get(index)} 입니다.")
+        }
+        */
+
+        /*
+        for(index in 0 .. 10 step 3) {
+            Log.d("For", "건너뛰기 : ${index}")
+        }
+        */
+
+        /*
+        for(index in 10 downTo 0) {
+            Log.d("For", "감소시키기 : ${index}")
+        }
+        */
+
+        /*
+        for(index in 10 downTo 0 step 3) {
+            Log.d("For", "건너뛰며 감소시키기 : ${index}")
+        }
+        */
+
+        var array = arrayOf("JAN", "FEB", "MAR", "APR", "MAY", "JUN")
+        for(month in array) {
+            Log.d("For", "현재 월은 ${month} 입니다.")
+        }
+    }
+}
