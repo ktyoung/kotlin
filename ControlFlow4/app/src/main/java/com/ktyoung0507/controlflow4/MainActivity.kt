133e8f301bf86946468b83881576c55262fedd56
ktyoung <ktyoung0507@gmail.com>
2022-10-06 11:51:35 +0900

    조건문 예제 4

--- ControlFlow4/app/src/main/java/com/ktyoung0507/controlflow4/MainActivity.kt
+++ ControlFlow4/app/src/main/java/com/ktyoung0507/controlflow4/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
         var now = 8
         when (now) {
             8 -> {
@@ -19,6 +20,20 @@ class MainActivity : AppCompatActivity() {
             }
             else -> {
                 Log.d("when", "현재 시간은 9시가 아닙니다.")
+            }
+        }
+        */
+
+        var ageOfMichael = 19
+        when (ageOfMichael) {
+            in 10..19 -> {
+                Log.d("when", "마이클은 10대입니다.")
+            }
+            !in 10..19 -> {
+                Log.d("when", "마이클은 10대가 아닙니다.")
+            }
+            else -> {
+                Log.d("when", "마이클의 나이를 알 수 없습니다.")
             }
         }
     }
