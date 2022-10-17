10ce2eb4e5e552d93ce25e828dc9899da693c517
ktyoung <ktyoung0507@gmail.com>
2022-10-17 12:55:13 +0900

    Seek Bar

---
+++ WidgetsSeekBar/app/src/main/java/com/ktyoung0507/widgetsseekbar/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ktyoung0507.widgetsseekbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import com.ktyoung0507.widgetsseekbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.textView.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+    }
+}
