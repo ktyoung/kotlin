f5e643def6d44c823150f5f104bac4b2b72a5687
ktyoung <ktyoung0507@gmail.com>
2022-10-18 12:45:01 +0900

    Activity

--- Activity/app/src/main/java/com/ktyoung0507/activity/MainActivity.kt
+++ Activity/app/src/main/java/com/ktyoung0507/activity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ktyoung0507.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.ktyoung0507.activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val message = it.data?.getStringExtra("returnValue")
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        }
 
         val intent = Intent(this, subActivity::class.java)
         intent.putExtra("from1", "Hello Bundle")
--- Activity/app/src/main/java/com/ktyoung0507/activity/subActivity.kt
+++ Activity/app/src/main/java/com/ktyoung0507/activity/subActivity.kt
@@ -1,5 +1,6 @@
 package com.ktyoung0507.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ktyoung0507.activity.databinding.ActivitySubBinding
@@ -13,5 +14,12 @@ class subActivity : AppCompatActivity() {
 
         binding.to1.text = intent.getStringExtra("from1")
         binding.to2.text = "${intent.getIntExtra("from2", 0)}"
+
+        binding.btnClose.setOnClickListener {
+            val returnIntent = Intent()
+            returnIntent.putExtra("returnValue", binding.editMessage.text.toString())
+            setResult(RESULT_OK, returnIntent)
+            finish()
+        }
     }
 }
