2244e254c7a21352e482b2a816452fea57a99d9d
ktyoung <ktyoung0507@gmail.com>
2022-10-20 12:26:43 +0900

    Fragment

--- Fragment/app/src/main/java/com/ktyoung0507/fragment/ListFragment.kt
+++ Fragment/app/src/main/java/com/ktyoung0507/fragment/ListFragment.kt
@@ -10,12 +10,14 @@ import com.ktyoung0507.fragment.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     var mainActivity: MainActivity? = null
+    lateinit var binding:FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentListBinding.inflate(inflater, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+
         binding.btnNext.setOnClickListener { mainActivity?.goDeatail() }
         binding.textTitle.text = arguments?.getString("key1")
         binding.textValue.text = "${arguments?.getInt("key2")}"
@@ -26,5 +28,9 @@ class ListFragment : Fragment() {
         super.onAttach(context)
 
         if (context is MainActivity) mainActivity = context
+    }
+
+    fun setValue(value:String) {
+        binding.textFromActivity.text = value
     }
 }
--- Fragment/app/src/main/java/com/ktyoung0507/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/ktyoung0507/fragment/MainActivity.kt
@@ -2,17 +2,27 @@ package com.ktyoung0507.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ktyoung0507.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    lateinit var listFragment: ListFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         setFragment()
+
+        binding.btnSend.setOnClickListener {
+            listFragment.setValue("전달할 값")
+        }
     }
 
     fun setFragment() {
-        val listFragment: ListFragment = ListFragment()
+        listFragment = ListFragment()
 
         var bundle = Bundle()
         bundle.putString("key1", "List Fragment")
@@ -26,7 +36,9 @@ class MainActivity : AppCompatActivity() {
 
     fun goDeatail() {
         val detailFragment = DetailFragment()
+
         val transaction = supportFragmentManager.beginTransaction()
+        
         transaction.add(R.id.frameLayout, detailFragment)
         transaction.addToBackStack("detail")
         transaction.commit()
