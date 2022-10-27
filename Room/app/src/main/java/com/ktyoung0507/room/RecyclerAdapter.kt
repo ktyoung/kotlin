3fd2b31bc23eda5ad96403dbccf5b3f56f55a701
ktyoung <ktyoung0507@gmail.com>
2022-10-27 12:38:34 +0900

    Room

---
+++ Room/app/src/main/java/com/ktyoung0507/room/RecyclerAdapter.kt
@@ -0,0 +1,43 @@
+package com.ktyoung0507.room
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ktyoung0507.room.databinding.ItemRecyclerBinding
+import java.text.SimpleDateFormat
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.Holder>() {
+    var helper:RoomHelper? = null
+    var listData = mutableListOf<RoomMemo>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
+    }
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val memo = listData.get(position)
+        holder.setRoomMemo(memo)
+    }
+
+    inner class Holder(val binding: ItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root) {
+        var mRoomMemo:RoomMemo? = null
+        init {
+            binding.buttonDelete.setOnClickListener {
+                helper?.roomMemoDao()?.delete(mRoomMemo!!)
+                listData.remove(mRoomMemo)
+                notifyDataSetChanged()
+            }
+        }
+        fun setRoomMemo(memo:RoomMemo) {
+            binding.textNo.text = "${memo.no}"
+            binding.textContent.text = memo.content
+            val sdf = SimpleDateFormat("yyyy/MM/dd hh:mm")
+            binding.textDatetime.text = "${sdf.format(memo.datetime)}"
+
+            this.mRoomMemo = memo
+        }
+    }
+}
