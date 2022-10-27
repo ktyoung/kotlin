783162ad17dd509973fcb8b018eb621c8664d91c
ktyoung <ktyoung0507@gmail.com>
2022-10-27 11:08:27 +0900

    SQLite

--- SQLite/app/src/main/java/com/ktyoung0507/sqlite/MainActivity.kt
+++ SQLite/app/src/main/java/com/ktyoung0507/sqlite/MainActivity.kt
@@ -2,10 +2,30 @@ package com.ktyoung0507.sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ktyoung0507.sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val helper = SqliteHelper(this, "memo", 1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val adapter = RecyclerAdapter()
+        adapter.listData.addAll(helper.selectMemo())
+        binding.recyclerMemo.adapter = adapter
+        binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
+        binding.buttonSave.setOnClickListener {
+            if (binding.editMemo.text.toString().isNotEmpty()) {
+                val memo = Memo(null, binding.editMemo.text.toString(), System.currentTimeMillis())
+                helper.insertMemo(memo)
+                adapter.listData.clear()
+                adapter.listData.addAll(helper.selectMemo())
+                adapter.notifyDataSetChanged()
+                binding.editMemo.setText("")
+            }
+        }
     }
 }
