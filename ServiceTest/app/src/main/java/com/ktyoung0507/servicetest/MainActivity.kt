ac4d9bb2e48a468fbb34d81a3d3084ae9e484d30
ktyoung <ktyoung0507@gmail.com>
2022-11-02 09:59:16 +0900

    Service

---
+++ ServiceTest/app/src/main/java/com/ktyoung0507/servicetest/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.ktyoung0507.servicetest
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun serviceStart(view: View) {
+        val intent = Intent(this, MyService::class.java)
+        intent.action = MyService.ACTION_START
+        startService(intent)
+    }
+
+    fun serviceStop(view: View) {
+        val intent = Intent(this, MyService::class.java)
+        stopService(intent)
+    }
+
+    var myService:MyService? = null
+    var isService = false
+    val connection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as MyService.MyBinder
+            myService = binder.getService()
+            isService = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isService = false
+        }
+    }
+
+    fun serviceBind(view: View) {
+        val intent = Intent(this, MyService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+    }
+
+    fun serviceUnbind(view: View) {
+        if(isService) {
+            unbindService(connection)
+            isService = false
+        }
+    }
+}
