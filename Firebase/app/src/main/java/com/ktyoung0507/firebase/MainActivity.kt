9655ed1bd5972950b989f977da5fcc05fb9e4777
ktyoung <ktyoung0507@gmail.com>
2022-11-07 10:42:47 +0900

    Firebase

--- Firebase/app/src/main/java/com/ktyoung0507/firebase/MainActivity.kt
+++ Firebase/app/src/main/java/com/ktyoung0507/firebase/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ktyoung0507.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+        myRef.setValue("Hello, Firebase!")
     }
 }
