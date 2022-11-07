e62365221d86e14688d78bf6cad48cd25faa809d
ktyoung <ktyoung0507@gmail.com>
2022-11-07 11:43:41 +0900

    Firebase

--- Firebase/app/src/main/java/com/ktyoung0507/firebase/MainActivity.kt
+++ Firebase/app/src/main/java/com/ktyoung0507/firebase/MainActivity.kt
@@ -8,24 +8,60 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.ktyoung0507.firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val database = Firebase.database("https://groovy-form-367500-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val myRef = database.getReference("users")
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val database = Firebase.database
-        val myRef = database.getReference("bbs")
+        fun addItem(user: User) {
+            val id = myRef.push().key!!
+            user.id = id
+            myRef.child(id).setValue(user)
+        }
+        with(binding){
+            btnPost.setOnClickListener {
+                val name = editName.text.toString()
+                val password = editPassword.text.toString()
+                val user = User(name, password)
+                addItem(user)
+            }
+        }
 
-        myRef.child("name").addValueEventListener(object: ValueEventListener {
+        myRef.addValueEventListener( object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                Log.d("파이어베이스", "${snapshot.value}")
-                print(snapshot.value)
+                with(binding) {
+                    textList.setText("")
+                    for (item in snapshot.children) {
+                        item.getValue(User::class.java)?.let { user ->
+                            textList.append("${user.name} : ${user.password} \n")
+                        }
+                    }
+                }
+
             }
 
             override fun onCancelled(error: DatabaseError) {
                 print(error.message)
             }
         })
+    }
+}
+
+class User {
+    var id:String = ""
+    var name: String = ""
+    var password: String = ""
+    constructor()
+
+    constructor(name:String, password:String) {
+        this.name = name
+        this.password = password
     }
 }
